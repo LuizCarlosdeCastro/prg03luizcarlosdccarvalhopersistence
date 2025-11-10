77cece341be8c72284f5530cbd85cb95e9659b72
Arkins <Arkins@DESKTOP-CACAMK7>
2025-11-10 00:03:23 -0300

    atividade 11 gerenciamento de curso com jpa hibernate

--- src/main/java/prg03luizcarlosdccarvalhopersistence/Prg03luizcarlosdccarvalhopersistence.java
+++ src/main/java/prg03luizcarlosdccarvalhopersistence/Prg03luizcarlosdccarvalhopersistence.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.prg03luizcarlosdccarvalhopersistence;
+package prg03luizcarlosdccarvalhopersistence;
 
 /**
  *
